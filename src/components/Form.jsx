39c2b25073507a5be4f05d7190e3c2d86c0292bd
Maria Júlia Miosso <mjuliamiosso@gmail.com>
2024-03-14 16:05:02 -0300

    adicionando formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,33 @@
+import styles from './Form.module.css'
 
 export const Form = () => {
-  return (
-    <div>
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        console.log(e)
+    }
 
-    </div>
+  return (
+    <form className={styles.form} onSubmit={handleFormSubmit}>
+        <h1>
+            Otimize seu tempo e se organize com o nosso<br></br>
+            <span>Planejador Diário!</span> 
+        </h1>
+        <div>
+            <input type="text" 
+            id='task' 
+            //value={task} 
+            // onInput={(e) => setTask(e.target.value)}
+            required
+            autoFocus
+            maxLength={60}
+            placeholder='Nova tarefa...' />
+            <button
+            aria-label='Add Task'
+            type='submit'
+            >+</button>
+        </div>
+        
+    </form>
   )
 }
 
